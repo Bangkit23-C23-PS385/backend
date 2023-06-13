5fa9de1c322cbbddb3b41f796ff2f4911399e35c
Rayhan Asadel <rayhan.asadel@tiket.com>
2023-06-13 23:34:10 +0700

    update db profile entity

--- src/entity/v1/db/profile/profile.go
+++ src/entity/v1/db/profile/profile.go
@@ -3,21 +3,21 @@ package profile
 import (
 	"time"
 
-	"ta/backend/src/constant"
-
-	"gorm.io/gorm"
+	"backend/src/constant"
 )
 
 type Profile struct {
-	gorm.Model
-	UserId      string              `gorm:"column:userId"`
+	// gorm.Model
+	UserId      string              `gorm:"column:userid;primaryKey"`
 	Name        string              `gorm:"column:name"`
 	Gender      constant.GenderType `gorm:"column:gender"`
-	DateOfBirth time.Time           `gorm:"column:dateOfBirth"`
+	DateOfBirth time.Time           `gorm:"column:dateofbirth"`
 	Height      int                 `gorm:"column:height"`
 	Weight      int                 `gorm:"column:weight"`
+	CreatedAt   time.Time           `gorm:"column:created_at"`
+	UpdatedAt   time.Time           `gorm:"column:updated_at"`
 }
 
 func (Profile) TableName() string {
-	return "profile"
+	return "profiles"
 }
